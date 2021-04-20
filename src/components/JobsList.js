9023b671ba70346b6b1496a081955a4f8d209185
NhatNam <namnhat1110@gmail.com>
2021-04-20 15:07:54 +0700

    add joblist

--- src/components/JobsList.js
+++ src/components/JobsList.js
@@ -1,30 +1,41 @@
 import React, { useState, useEffect } from 'react'
+import { Card } from 'react-bootstrap'
 
 const JobsList = () => {
     const [jobList, setJobList] = useState([]);
 
-    // useEffect(() => {
-    //     const fetchData = async () => {
-    //         //code here
-    //         // your URL is http://localhost:3001/jobs, but you should use the env variable, so something like:
-    //         // const url = `${REACT_APP_BACKEND_SERVER_URL}/jobs`
-    //     };
-    //     fetchData();
-    // }, []);
 
     useEffect(() => {
         async function FetchJobs() {
             const response = await fetch('http://localhost:3001/jobs')
             const json = await response.json()
-            console({ json })
+            console.log({ json })
+            setJobList(json)
         }
         FetchJobs()
     }, [])
 
     return (
-        <div>All jobs here</div>
+        <div>
+            <h1>All jobs here</h1>
+            {jobList.map(j => {
+                return (
+                    <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                            <Card.Title>Card Title</Card.Title>
+                            <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                            <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            <Card.Link href="#">Card Link</Card.Link>
+                            <Card.Link href="#">Another Link</Card.Link>
+                        </Card.Body>
+                    </Card>
+                )
+            })}
+
+        </div>
     )
-
 }
-
 export default JobsList
